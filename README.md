c23509a15fb74d3cb985cb9f4e7ff3f23408b2c4
jcrvlh <jc.jeffcarvalho@gmail.com>
2016-06-06 23:07:52 -0300

    Teste 1
    Teste 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Api.ai - sample webhook implementation in Python
+
+Teste 1 de webhook do Aleatório feito em Python
---
+++ app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "novolink":
+        return {}
+    baseurl = "http://aleatorio.jeffcarvalho.com.br/getlink.php?"
+    yql_url = baseurl
+    result = urllib.urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeWebhookResult(data):
+
+    link = data.get('url')
+    if link is None:
+        return {}
+
+    speech = link
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "aleatorio-getlink"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=False, port=port, host='0.0.0.0')
